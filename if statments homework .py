83e33542c01d1dd14f1e5c662e141ddd9eaef808
omichael <omichaeltmichael@live.co.uk>
2021-08-10 10:15:37 +0100

    Genisis

---
+++ if statments homework .py
@@ -0,0 +1,30 @@
+##list of movies with d
+movies = {"Spiderman": {'release Date': 2002,  "rating":"PG"},
+          "shrek": {'release Date': 2001,  "rating":"PG-13"},
+          "mission impossible": {'release Date': 1996,"rating":"PG-13"},
+          }
+
+##### print the list of movies ###
+print("movie list",movies.keys())
+#### ask the user for input ###
+movie=input("what movie do you want see from the list \n")
+### make sure that the movie is within the list ###
+while(movie not in movies.keys()):
+    print("Try again, movie does not exist")
+    movie = input("what movie do you want see from the list \n")
+
+### another input ###
+valid=True
+
+while valid:
+    age = input("your age Please \n")
+    
+## the if section ##
+statment= f"you can watch {movie}. Rated:{movies[movie]['rating']}. have a fun time"
+
+if movies[movie]["rating"] == "PG" and int(age)> 10:
+    print(statment)
+elif movies[movie]["rating"] == "PG-13" and int(age) > 13:
+    print(statment)
+else:
+    print("try again mate, you are too young")
